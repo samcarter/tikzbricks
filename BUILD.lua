ff09e8355ddbcd13bea6cd8cc4ab7926e2ce4286
samcarter <sam.carter8@aol.com>
2024-04-25 17:20:28 +0200

    doc: simplified footnotes in tcolorbox titles

--- BUILD.lua
+++ BUILD.lua
@@ -1,17 +1,18 @@
 #!/usr/bin/env texlua
 
--- Execute with ======================================================
+-- Execute with ================================================================
 -- l3build tag
 -- l3build ctan
 -- <check announcement.txt and format if necessary>
 -- l3build upload
 -- l3build clean
 
--- Settings ==========================================================
+-- Settings ====================================================================
 module = "tikzbricks"
 ctanpkg = "tikzbricks"
 ctanprefix = "/graphics/pgf/contrib/" 
 ctansummary = "A small LaTeX package to draw bricks with TikZ"
 
 -- common settings =============================================================
+-- https://github.com/samcarter/beamertheme-sam/blob/main/build-settings.lua
 local common_settings, build_settings = pcall(require, "../beamertheme-sam/build-settings.lua")
